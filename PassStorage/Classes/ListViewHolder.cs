fed1e6617e4e45520ecf9ccfdba35bd74fe7bb1b
DonRatello <marcin@mczernecki.pl>
2014-12-26 14:47:19 +0100

    ading untracked files

---
+++ PassStorage/Classes/ListViewHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassStorage.Classes
+{
+    class ListViewHolder
+    {
+        public string Name;
+        public string Value;
+    }
+}
